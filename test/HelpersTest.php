fb9016f797587769f4c1bd4f26023b0c4d9be462
Simo Niemelä <simo.niemela@gmail.com>
2011-04-11 00:09:56 +0300

    Fix a require path in the HelpersTest

--- test/HelpersTest.php
+++ test/HelpersTest.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once '../src/HamlPHP/Helpers.php';
+require_once HAMLPHP_DIR_SRC . '/Helpers.php';
 require_once 'test_helper.php';
 
 class HelpersTest extends PHPUnit_Framework_TestCase
@@ -9,7 +9,7 @@ class HelpersTest extends PHPUnit_Framework_TestCase
 
   public function __construct()
   {
-    $this->compiler = new Compiler();
+    $this->compiler = getTestCompiler();
   }
 
   public function testAttributeFunction()
