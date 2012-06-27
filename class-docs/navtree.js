36df7f6f3e86a862b4616f77265818915dc57014
Saulo Vallory <me@saulovallory.com>
2012-06-27 19:08:27 -0300

    Adds class api docs

---
+++ class-docs/navtree.js
@@ -0,0 +1,358 @@
+var NAVTREE =
+[
+  [ "HamlPHP", "index.html", [
+    [ "Related Pages", "pages.html", [
+      [ "Todo List", "todo.html", null ]
+    ] ],
+    [ "Data Structures", "annotated.html", [
+      [ "BaseException", "class_base_exception.html", null ],
+      [ "BaseObject", "class_base_object.html", null ],
+      [ "CommentNode", "class_comment_node.html", null ],
+      [ "Compiler", "class_compiler.html", null ],
+      [ "Config", "class_config.html", null ],
+      [ "ContentEvaluator", "interface_content_evaluator.html", null ],
+      [ "CssFilter", "class_css_filter.html", null ],
+      [ "DefaultContentEvaluator", "class_default_content_evaluator.html", null ],
+      [ "DoctypeNode", "class_doctype_node.html", null ],
+      [ "Element", "class_element.html", null ],
+      [ "ElementNode", "class_element_node.html", null ],
+      [ "FileStorage", "class_file_storage.html", null ],
+      [ "Filter", "interface_filter.html", null ],
+      [ "FilterContainer", "class_filter_container.html", null ],
+      [ "FilterNode", "class_filter_node.html", null ],
+      [ "HamlNode", "class_haml_node.html", null ],
+      [ "HamlPHP", "class_haml_p_h_p.html", null ],
+      [ "Interpolation", "class_interpolation.html", null ],
+      [ "InvalidTagException", "class_invalid_tag_exception.html", null ],
+      [ "JavascriptFilter", "class_javascript_filter.html", null ],
+      [ "NodeFactory", "class_node_factory.html", null ],
+      [ "PhpFilter", "class_php_filter.html", null ],
+      [ "PlainFilter", "class_plain_filter.html", null ],
+      [ "ReadOnlyPropertyException", "class_read_only_property_exception.html", null ],
+      [ "RootNode", "class_root_node.html", null ],
+      [ "Storage", "interface_storage.html", null ],
+      [ "StringScanner", "class_string_scanner.html", null ],
+      [ "SyntaxErrorException", "class_syntax_error_exception.html", null ],
+      [ "TagNode", "class_tag_node.html", null ],
+      [ "UndefinedPropertyException", "class_undefined_property_exception.html", null ]
+    ] ],
+    [ "Data Structure Index", "classes.html", null ],
+    [ "Class Hierarchy", "hierarchy.html", [
+      [ "BaseException", "class_base_exception.html", [
+        [ "ReadOnlyPropertyException", "class_read_only_property_exception.html", null ],
+        [ "UndefinedPropertyException", "class_undefined_property_exception.html", null ]
+      ] ],
+      [ "BaseObject", "class_base_object.html", [
+        [ "StringScanner", "class_string_scanner.html", null ]
+      ] ],
+      [ "Compiler", "class_compiler.html", null ],
+      [ "Config", "class_config.html", null ],
+      [ "ContentEvaluator", "interface_content_evaluator.html", [
+        [ "DefaultContentEvaluator", "class_default_content_evaluator.html", null ],
+        [ "FileStorage", "class_file_storage.html", null ]
+      ] ],
+      [ "Element", "class_element.html", null ],
+      [ "Filter", "interface_filter.html", [
+        [ "CssFilter", "class_css_filter.html", null ],
+        [ "JavascriptFilter", "class_javascript_filter.html", null ],
+        [ "PhpFilter", "class_php_filter.html", null ],
+        [ "PlainFilter", "class_plain_filter.html", null ]
+      ] ],
+      [ "FilterContainer", "class_filter_container.html", null ],
+      [ "HamlPHP", "class_haml_p_h_p.html", null ],
+      [ "Interpolation", "class_interpolation.html", null ],
+      [ "InvalidTagException", "class_invalid_tag_exception.html", null ],
+      [ "NodeFactory", "class_node_factory.html", null ],
+      [ "RootNode", "class_root_node.html", [
+        [ "HamlNode", "class_haml_node.html", [
+          [ "CommentNode", "class_comment_node.html", null ],
+          [ "DoctypeNode", "class_doctype_node.html", null ],
+          [ "ElementNode", "class_element_node.html", null ],
+          [ "FilterNode", "class_filter_node.html", null ],
+          [ "TagNode", "class_tag_node.html", null ]
+        ] ]
+      ] ],
+      [ "Storage", "interface_storage.html", [
+        [ "FileStorage", "class_file_storage.html", null ]
+      ] ],
+      [ "SyntaxErrorException", "class_syntax_error_exception.html", null ]
+    ] ],
+    [ "Data Fields", "functions.html", null ],
+    [ "File List", "files.html", [
+      [ "/projects/wp-haml/hamlphp/src/HamlPHP/CommentNode.php", "_comment_node_8php.html", null ],
+      [ "/projects/wp-haml/hamlphp/src/HamlPHP/Compiler.php", "_compiler_8php.html", null ],
+      [ "/projects/wp-haml/hamlphp/src/HamlPHP/Config.php", "_config_8php.html", null ],
+      [ "/projects/wp-haml/hamlphp/src/HamlPHP/DoctypeNode.php", "_doctype_node_8php.html", null ],
+      [ "/projects/wp-haml/hamlphp/src/HamlPHP/Element.php", "_element_8php.html", null ],
+      [ "/projects/wp-haml/hamlphp/src/HamlPHP/ElementNode.php", "_element_node_8php.html", null ],
+      [ "/projects/wp-haml/hamlphp/src/HamlPHP/FilterNode.php", "_filter_node_8php.html", null ],
+      [ "/projects/wp-haml/hamlphp/src/HamlPHP/HamlNode.php", "_haml_node_8php.html", null ],
+      [ "/projects/wp-haml/hamlphp/src/HamlPHP/HamlPHP.php", "_haml_p_h_p_8php.html", null ],
+      [ "/projects/wp-haml/hamlphp/src/HamlPHP/Helpers.php", "_helpers_8php.html", null ],
+      [ "/projects/wp-haml/hamlphp/src/HamlPHP/Interpolation.php", "_interpolation_8php.html", null ],
+      [ "/projects/wp-haml/hamlphp/src/HamlPHP/NodeFactory.php", "_node_factory_8php.html", null ],
+      [ "/projects/wp-haml/hamlphp/src/HamlPHP/RootNode.php", "_root_node_8php.html", null ],
+      [ "/projects/wp-haml/hamlphp/src/HamlPHP/TagNode.php", "_tag_node_8php.html", null ],
+      [ "/projects/wp-haml/hamlphp/src/HamlPHP/ContentEvaluator/ContentEvaluator.php", "_content_evaluator_8php.html", null ],
+      [ "/projects/wp-haml/hamlphp/src/HamlPHP/ContentEvaluator/DefaultContentEvaluator.php", "_default_content_evaluator_8php.html", null ],
+      [ "/projects/wp-haml/hamlphp/src/HamlPHP/Filter/CssFilter.php", "_css_filter_8php.html", null ],
+      [ "/projects/wp-haml/hamlphp/src/HamlPHP/Filter/Filter.php", "_filter_8php.html", null ],
+      [ "/projects/wp-haml/hamlphp/src/HamlPHP/Filter/FilterContainer.php", "_filter_container_8php.html", null ],
+      [ "/projects/wp-haml/hamlphp/src/HamlPHP/Filter/JavascriptFilter.php", "_javascript_filter_8php.html", null ],
+      [ "/projects/wp-haml/hamlphp/src/HamlPHP/Filter/PhpFilter.php", "_php_filter_8php.html", null ],
+      [ "/projects/wp-haml/hamlphp/src/HamlPHP/Filter/PlainFilter.php", "_plain_filter_8php.html", null ],
+      [ "/projects/wp-haml/hamlphp/src/HamlPHP/Storage/FileStorage.php", "_file_storage_8php.html", null ],
+      [ "/projects/wp-haml/hamlphp/src/HamlPHP/Storage/Storage.php", "_storage_8php.html", null ],
+      [ "/projects/wp-haml/hamlphp/src/HamlPHP/Util/BaseException.php", "_base_exception_8php.html", null ],
+      [ "/projects/wp-haml/hamlphp/src/HamlPHP/Util/BaseObject.php", "_base_object_8php.html", null ],
+      [ "/projects/wp-haml/hamlphp/src/HamlPHP/Util/StringScanner.php", "_string_scanner_8php.html", null ]
+    ] ],
+    [ "Globals", "globals.html", null ]
+  ] ]
+];
+
+function createIndent(o,domNode,node,level)
+{
+  if (node.parentNode && node.parentNode.parentNode)
+  {
+    createIndent(o,domNode,node.parentNode,level+1);
+  }
+  var imgNode = document.createElement("img");
+  if (level==0 && node.childrenData)
+  {
+    node.plus_img = imgNode;
+    node.expandToggle = document.createElement("a");
+    node.expandToggle.href = "javascript:void(0)";
+    node.expandToggle.onclick = function() 
+    {
+      if (node.expanded) 
+      {
+        $(node.getChildrenUL()).slideUp("fast");
+        if (node.isLast)
+        {
+          node.plus_img.src = node.relpath+"ftv2plastnode.png";
+        }
+        else
+        {
+          node.plus_img.src = node.relpath+"ftv2pnode.png";
+        }
+        node.expanded = false;
+      } 
+      else 
+      {
+        expandNode(o, node, false);
+      }
+    }
+    node.expandToggle.appendChild(imgNode);
+    domNode.appendChild(node.expandToggle);
+  }
+  else
+  {
+    domNode.appendChild(imgNode);
+  }
+  if (level==0)
+  {
+    if (node.isLast)
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2plastnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2lastnode.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+    else
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2pnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2node.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+  }
+  else
+  {
+    if (node.isLast)
+    {
+      imgNode.src = node.relpath+"ftv2blank.png";
+    }
+    else
+    {
+      imgNode.src = node.relpath+"ftv2vertline.png";
+    }
+  }
+  imgNode.border = "0";
+}
+
+function newNode(o, po, text, link, childrenData, lastNode)
+{
+  var node = new Object();
+  node.children = Array();
+  node.childrenData = childrenData;
+  node.depth = po.depth + 1;
+  node.relpath = po.relpath;
+  node.isLast = lastNode;
+
+  node.li = document.createElement("li");
+  po.getChildrenUL().appendChild(node.li);
+  node.parentNode = po;
+
+  node.itemDiv = document.createElement("div");
+  node.itemDiv.className = "item";
+
+  node.labelSpan = document.createElement("span");
+  node.labelSpan.className = "label";
+
+  createIndent(o,node.itemDiv,node,0);
+  node.itemDiv.appendChild(node.labelSpan);
+  node.li.appendChild(node.itemDiv);
+
+  var a = document.createElement("a");
+  node.labelSpan.appendChild(a);
+  node.label = document.createTextNode(text);
+  a.appendChild(node.label);
+  if (link) 
+  {
+    a.href = node.relpath+link;
+  } 
+  else 
+  {
+    if (childrenData != null) 
+    {
+      a.className = "nolink";
+      a.href = "javascript:void(0)";
+      a.onclick = node.expandToggle.onclick;
+      node.expanded = false;
+    }
+  }
+
+  node.childrenUL = null;
+  node.getChildrenUL = function() 
+  {
+    if (!node.childrenUL) 
+    {
+      node.childrenUL = document.createElement("ul");
+      node.childrenUL.className = "children_ul";
+      node.childrenUL.style.display = "none";
+      node.li.appendChild(node.childrenUL);
+    }
+    return node.childrenUL;
+  };
+
+  return node;
+}
+
+function showRoot()
+{
+  var headerHeight = $("#top").height();
+  var footerHeight = $("#nav-path").height();
+  var windowHeight = $(window).height() - headerHeight - footerHeight;
+  navtree.scrollTo('#selected',0,{offset:-windowHeight/2});
+}
+
+function expandNode(o, node, imm)
+{
+  if (node.childrenData && !node.expanded) 
+  {
+    if (!node.childrenVisited) 
+    {
+      getNode(o, node);
+    }
+    if (imm)
+    {
+      $(node.getChildrenUL()).show();
+    } 
+    else 
+    {
+      $(node.getChildrenUL()).slideDown("fast",showRoot);
+    }
+    if (node.isLast)
+    {
+      node.plus_img.src = node.relpath+"ftv2mlastnode.png";
+    }
+    else
+    {
+      node.plus_img.src = node.relpath+"ftv2mnode.png";
+    }
+    node.expanded = true;
+  }
+}
+
+function getNode(o, po)
+{
+  po.childrenVisited = true;
+  var l = po.childrenData.length-1;
+  for (var i in po.childrenData) 
+  {
+    var nodeData = po.childrenData[i];
+    po.children[i] = newNode(o, po, nodeData[0], nodeData[1], nodeData[2],
+        i==l);
+  }
+}
+
+function findNavTreePage(url, data)
+{
+  var nodes = data;
+  var result = null;
+  for (var i in nodes) 
+  {
+    var d = nodes[i];
+    if (d[1] == url) 
+    {
+      return new Array(i);
+    }
+    else if (d[2] != null) // array of children
+    {
+      result = findNavTreePage(url, d[2]);
+      if (result != null) 
+      {
+        return (new Array(i).concat(result));
+      }
+    }
+  }
+  return null;
+}
+
+function initNavTree(toroot,relpath)
+{
+  var o = new Object();
+  o.toroot = toroot;
+  o.node = new Object();
+  o.node.li = document.getElementById("nav-tree-contents");
+  o.node.childrenData = NAVTREE;
+  o.node.children = new Array();
+  o.node.childrenUL = document.createElement("ul");
+  o.node.getChildrenUL = function() { return o.node.childrenUL; };
+  o.node.li.appendChild(o.node.childrenUL);
+  o.node.depth = 0;
+  o.node.relpath = relpath;
+
+  getNode(o, o.node);
+
+  o.breadcrumbs = findNavTreePage(toroot, NAVTREE);
+  if (o.breadcrumbs == null)
+  {
+    o.breadcrumbs = findNavTreePage("index.html",NAVTREE);
+  }
+  if (o.breadcrumbs != null && o.breadcrumbs.length>0)
+  {
+    var p = o.node;
+    for (var i in o.breadcrumbs) 
+    {
+      var j = o.breadcrumbs[i];
+      p = p.children[j];
+      expandNode(o,p,true);
+    }
+    p.itemDiv.className = p.itemDiv.className + " selected";
+    p.itemDiv.id = "selected";
+    $(window).load(showRoot);
+  }
+}
+
